d6d77aa518499cfe8d930ffdcf2969a822fe4893
DanielQuilcue <danielquilcue18@gmail.com>
2024-02-14 00:41:05 -0500

    update Form

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import NuevoCliente, { action as nuevoClienteAction }from "./pages/NuevoCliente";
+import Index, { Loader as clientesLoader } from "./pages/Index";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Inicio</h1>,
+    element: <Layout />, 
+    children: [
+      {
+        index: true,
+        element:<Index />,
+        loader: clientesLoader
+      },
+      {
+        path: '/clientes/nuevo',
+        element:<NuevoCliente />,
+        action: nuevoClienteAction
+      }
+    ]
   },
-  {
-
-  }
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
